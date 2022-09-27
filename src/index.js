c7c8216e732abca323047ae97a6d6ff08fc7b9ae
JairoDuenas <jairoduenas.ing@gmail.com>
2022-09-27 17:12:57 -0500

    Error corregido

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const composeAlt = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+const composeAlt = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const composedEnhancers = composeAlt(applyMiddleware(thunk, logger));
 
